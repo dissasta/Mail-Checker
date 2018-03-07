0b2bf76bb7fe99be53bab5918c5e0d5777cb99e1
dissasta <kapuscikjan@gmail.com>
2018-03-07 21:40:44 +0000

    second commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Tkinter import *
 import tkFileDialog
 import os
+import time
 globFont = "Verdana 8 bold"
 
 class EntryBox(object):
@@ -13,6 +14,9 @@ class EntryBox(object):
 class Main_Window(Frame):
 	def __init__(self, master):
 		Frame.__init__(self, master, relief=SUNKEN, bd=2)
+		logFile = open('log.txt', 'w')
+		logFile.close()
+		self.master = master
 		self.hdEntryText = 'sadas'
 		self.log = []
 		self.rows = 1
@@ -33,34 +37,42 @@ class Main_Window(Frame):
 	def createWindow(self):
 		self.mainFrame = Canvas(width=680, height=400, bg="GREY")
 		self.mainFrame.pack()
-		self.fnButton = Button(self.mainFrame, text = 'Wybierz plik wsadowy', anchor = W, command=self.openFile)
+		self.fnButton = Button(self.mainFrame, text = 'Choose input file', anchor = W, command=self.openFile)
 		self.fnLabel = Label(self.mainFrame, text='...', anchor = W)
-		self.hdLabel = Label(self.mainFrame, text='Dodatkowe zapytania:', anchor = W)
+		self.hdLabel = Label(self.mainFrame, text='Additional queries:', anchor = W)
 		self.hdEntry = Text(self.mainFrame, height = 10, width = 20, background = '#424242', foreground = "#2195E7")
-		self.statLabel = Text(self.mainFrame, height = 10, width = 91, background = '#424242', foreground = "#2195E7", state=DISABLED)
-		#self.fnLabel.configure(relief = FLAT)
+		self.statLabel = Text(self.mainFrame, height = 10, width = 91, background = '#424242', state=DISABLED)
+		self.statLabel.tag_config('OK', foreground='GREEN')
+		self.statLabel.tag_config('WARN', foreground='RED')
+		self.statLabel.tag_config('NA', foreground='#2195E7')
+
 		self.fileButton = self.mainFrame.create_window(20, 20, anchor = NW, window = self.fnButton)
-		self.fileName = self.mainFrame.create_window(150, 22, anchor = NW, window = self.fnLabel)
-		self.headerLabel = self.mainFrame.create_window(400, 22, anchor = NW, window = self.hdLabel)
+		self.fileName = self.mainFrame.create_window(125, 22, anchor = NW, window = self.fnLabel)
+		self.headerLabel = self.mainFrame.create_window(420, 22, anchor = NW, window = self.hdLabel)
 		self.headerEntry = self.mainFrame.create_window(520, 22, anchor = NW, window = self.hdEntry)
 		self.statusEntry = self.mainFrame.create_window(22, 230, anchor = NW, window = self.statLabel)
+
+
 
 	def openFile(self):
 		self.openFile = tkFileDialog.askopenfilename(filetypes = (('text files', '*.txt'),))
 		if self.openFile:
 			self.fnLabel.config(text = '...' + self.openFile.split('/')[-1])
 			print self.hdEntry.get('1.0', END)
-			self.writeToLog('Otworzono plik wsadowy: ' + self.openFile)
+			self.writeToLog('INPUT FILE OPEN: ' + self.openFile, 'NA')
 			self.scanFile(self.openFile)
 
 	def scanFile(self, file):
+		emailCount = 0
 		with open(file, 'r') as plik:
 			for email in plik.readlines():
 				try:
 					mail = email.rstrip().split('@')
 					if len(mail) == 2:
 						if len(mail[-1].split('.')) >= 2:
-							self.writeToLog('Zaimportowano: ' + email.rstrip())
+							self.writeToLog('Imported: ' + email.rstrip(), 'NA')
+							self.logToFile('Imported: ' + email.rstrip())
+							emailCount += 1
 							if mail[-1] in self.emailDir:
 								#if domain already exists in the directory, add the additional account to the list. Only if it doesn't exist yet.
 								if not mail[0] in self.emailDir[mail[-1]][0]:
@@ -72,24 +84,34 @@ class Main_Window(Frame):
 				except Exception:
 					print 'something went wrong'
 
+		self.writeToLog('Total emails imported: ' + str(emailCount), 'OK')
 		print self.emailDir
 
-	def writeToLog(self, text):
+	def writeToLog(self, text, status):
 		if len(self.log) >= 10:
 			self.log.pop(0)
 
-		self.log.append(text)
+		self.log.append((text, status))
+
 		self.statLabel.config(state=NORMAL)
 		self.statLabel.delete('1.0', END)
 		for i in range(len(self.log)):
 			if i == 9:
-				self.statLabel.insert(END, self.log[i])
+				self.statLabel.insert(END, self.log[i][0], self.log[i][1])
+
 			else:
-				self.statLabel.insert(END, self.log[i] + '\n')
+				self.statLabel.insert(END, self.log[i][0] + '\n', self.log[i][1])
+
 		self.statLabel.config(state=DISABLED)
+		time.sleep(0.005)
+		self.master.update()
 
 	def updateStat(self):
 		pass
+
+	def logToFile(self, text):
+		log = open('log.txt', 'a')
+		log.write(text + '\n')
 
 	def saveToFile(self):
 		f = tkFileDialog.asksaveasfile(mode='w', defaultextension=".stk")
